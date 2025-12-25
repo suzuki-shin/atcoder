da2f79de9da6b7f18346f8951a44ac5ae832bb65
Shinichiro Suzuki <shinichiro.su@gmail.com>
2025-12-26 06:23:31 +0900

    Vectorで二分探索しなくてもIntSet.memberで十分間に合う

--- tessoku-book/app/a14/Main.hs
+++ tessoku-book/app/a14/Main.hs
@@ -61,14 +61,8 @@ type Solver = Dom -> Codom
 solve :: Solver
 solve (_,k,as,bs,cs,ds) =
   let ps = [a + b | a <- as, b <- bs, a + b < k]
-      qs = [c + d | c <- cs, d <- ds, c + d < k]
-      qv = VU.modify (VAI.sortBy compare) $ VU.fromList qs
-      lenQs = vLength qv
-      -- qsの中にp + qi == kとなるqiがあるか？(k-p以上の値を持つ最小のindexがあり、かつそれがk-pと一致するか？)
-      check p =
-        let idx = AB.minLeft 0 lenQs (\i -> p + qv VG.! i >= k)
-        in idx < lenQs && qv VG.! idx == k - p
-  in yn $ any check ps
+      qs = IS.fromList [c + d | c <- cs, d <- ds, c + d < k]
+  in yn $ any (\p -> IS.member (k - p) qs) ps
 
 
 {-# INLINE decode #-}
